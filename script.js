4b23228c9fba701d52aec37cbc3b565047009616
Sheikh <kenneth.sheikh@hotmail.com>
2024-02-09 12:25:54 +0100

    added catch testing examples in script

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ try {
   // Optional: Code that will always be executed, whether there is an exception or not
   console.log("This will always be executed");
 }
+
+// another form of testing
+
+try {
+  const url = "someurl.com";
+  url = "";
+  new date("2014-25-23").toISOString();
+  cnsole.log("finished no errors");
+} catch (error) {
+  console.log("something went wrong");
+}
+
+// try testing step in sources, this shows how it goes thru the code
